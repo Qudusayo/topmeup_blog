c8df8a9644141d59a573ba990c6d13d654265995
Qudusayo <zeekimono@gmail.com>
2020-12-31 23:59:43 +0100

    Test Preview

---
+++ content/blog/making-cool-cash/index.md
@@ -0,0 +1,22 @@
+---
+title: How to make money on TopUpLab
+date: 2021-01-01 00:00:00 +0300
+description: # Add post description (optional)
+img: ./money.jpg # Making Cool Cash (optional)
+tags: [Making Money Online, TopUpLab, Making Money On TopUpLab] # add tag
+---
+
+### How to make money on TopUpLab
+
+Let's take for instance… Qudusayo, a member of [TopUpLab](https://topuplab.com.ng), buys data from us at N300 per gigabyte, as a student, he decides to be retailing the data plans by selling to her colleagues at N350 or N400 per gig depending on the price he wishes to sell. By so doing, she makes N65 or N105 per transaction.  
+
+If he has as little as 10 transactions per day, that will make him N650 or N1050 respectively on daily basis. Now, N1050 * 30 days is equivalent to N31500 monthly. A month as profit without any stress.  
+
+Remember that this is for Data selling alone, imagine if Qudusayo was also retailing other products like Waec PINs, or Cable(DsTV subscriptions). He may be making N150,000 a month in profits. That is N5000 in profits everyday.  
+
+How do I get started and begin making money with this offer ? It's quite easy: 
+- Just register on [TopUpLab](topuplab.com.ng)  
+- Fund your wallet via card or transfer.  
+- Then you can buy data or other products for yourself or anyone else at anytime, any day.  
+
+HAPPY TRANSACTION 
---
+++ gatsby-config.js
@@ -0,0 +1,80 @@
+module.exports = {
+  siteMetadata: {
+    title: `TopUpLab`,
+    description: `I am a web developer focusing on front-end development. Always hungry to keep learning.`,
+    author: `TopUpLab`,
+    siteUrl: `https://topuplab.com.ng`,
+    social: {
+      twitter: `topuplab`,
+      facebook: `topuplab`,
+      email: `topuplab@gmail.com`,
+    },
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/blog`,
+        name: `blog`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/content/assets`,
+        name: `assets`,
+      },
+    },
+    {
+      resolve: `gatsby-transformer-remark`,
+      options: {
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 970,
+            },
+          },
+          {
+            resolve: `gatsby-remark-katex`,
+            options: {
+              strict: `warn`,
+            }
+          },
+          {
+            resolve: `gatsby-remark-copy-linked-files`,
+            options: {
+              destinationDir: `files`,
+              ignoreFileExtensions: [`png`, `jpg`, `jpeg`, `bmp`, `tiff`],
+            },
+          },
+          `gatsby-remark-prismjs`,
+        ],
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        //trackingId: `ADD YOUR TRACKING ID HERE`,
+      },
+    },
+    `gatsby-plugin-feed`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `Top Up Lab`,
+        short_name: `TopUpLab`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `./static/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    // `gatsby-plugin-offline`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`,
+  ],
+}
